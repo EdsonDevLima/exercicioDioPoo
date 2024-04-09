aed4df91cd5ce2e6b617b929b5144283ac07f921
Edson Lima <145128715+EdsonDevLima@users.noreply.github.com>
2024-04-09 07:41:34 -0300

    commit end

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DesafioPOO.Models;
+
+Nokia meuNokia = new Nokia("tijolao","1475v","1dqwd4568",1);
+meuNokia.Ligar();
+meuNokia.ReceberLigacao();
+meuNokia.InstalarAplicativo("whatzapp");
+Iphone meuIphone = new Iphone("iphone 5","12314v","1e45efwf1",2);
+meuIphone.Ligar();
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("whatzapp");
